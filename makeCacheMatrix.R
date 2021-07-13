dc05630540af306e3df537006589222dd35a6edd
Yun Liu <84511205+YunL-LY@users.noreply.github.com>
2021-07-13 12:40:27 +0100

    Week 3_Programming Assignment 2: Lexical Scoping
    update the comments

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,21 +1,21 @@
 makeCacheMarix <- function(x = matrix()){
         inv <- NULL
         set <- function(y){
-                x <<- y
+                x <<- y  ## set the value
                 inv <<- NULL
         }
         get <- function() {x}
         setinverse <- function(inverse) {inv <<- inverse}
         getinverse <- function() {inv}
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-}
+}  ## return the frame in matrix
 
 cacheSolve <- function(x, ...){
        inv <- x$getinverse()
        if(!is.null(inv)){
                message("getting cached data")
          return(inv)
-       }
+       } ## check 
        mat <- x$get()
        inv <- solve(mat, ...)
        inv
